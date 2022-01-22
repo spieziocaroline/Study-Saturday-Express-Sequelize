20077f45971514656e7fe4424467fce950d2b257
Caroline Spiezio <spieziocaroline@gmail.com>
2022-01-21 21:37:41 -0500

    passing specs for cycle 2

--- db/models/test.js
+++ db/models/test.js
@@ -2,6 +2,18 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define();
+const Test = db.define('test', {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
+
+Test.belongsTo(Student);
+Student.hasMany(Test);
 
 module.exports = Test;
